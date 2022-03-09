99904014241aea6a746773c37fe465ef36a14ecf
88567982@qq.com <88567982@qq.com>
2022-03-09 21:46:12 +0800

    111

--- study/godesignbook/ch7/interface_test.go
+++ study/godesignbook/ch7/interface_test.go
@@ -10,5 +10,12 @@ import (
 var _ io.Writer = (*bytes.Buffer)(nil)
 
 func TestInterface(t *testing.T) {
-
+	contrys := make(map[string]string)
+	contrys["china"] = "中国"
+	contrys["english"] = "英国"
+	contrys["france"] = "法国"
+	for k, v := range contrys {
+		t.Logf("%s=%s ,", k, v)
+	}
+	t.Logf("变得聪明不是挺好的选择么")
 }
