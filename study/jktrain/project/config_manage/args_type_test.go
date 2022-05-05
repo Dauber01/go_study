d86a5cc1f4d28a1081182f388ab1099bc066c88c
88567982@qq.com <88567982@qq.com>
2022-05-05 23:05:12 +0800

    1

--- study/jktrain/project/config_manage/args_type_test.go
+++ study/jktrain/project/config_manage/args_type_test.go
@@ -1,42 +1,41 @@
-package project_tmp_test
+package config_manage_test
 
 import (
-	"testing"
 	"fmt"
+	"testing"
 )
 
-type Option interface{
+type Option interface {
 	Hello() error
 	Bye() error
 }
 
-type Demo struct{
-
+type Demo struct {
 }
 
-func (d *Demo)Hello() error {
+func (d *Demo) Hello() error {
 	fmt.Println("hello")
 	return nil
 }
 
-func (d *Demo)Bye() error {
+func (d *Demo) Bye() error {
 	fmt.Println("Bye")
 	return nil
 }
 
-type ShowDemo struct{
+type ShowDemo struct {
 	Demo
 	count int
 }
 
-func ArgsType(opthions ...Option){
-	for _, op := range opthions{
+func ArgsType(opthions ...Option) {
+	for _, op := range opthions {
 		op.Hello()
 	}
 }
 
 // 可以看到对于 ...Option 类型的参数,它的实现 struct 的包装 struct 也可以作为参数
-func TestArgsType(t *testing.T){
+func TestArgsType(t *testing.T) {
 	sd := &ShowDemo{}
 	ArgsType(sd)
 }
